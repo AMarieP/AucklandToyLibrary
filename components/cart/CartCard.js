15211d93b6b7bfbe5c2e39f291ba846238e32db9
Alyssa <alyssa@kvasl.com>
2023-04-22 21:31:00 +1200

    Finisehd qty and cart card components

--- components/cart/CartCard.js
+++ components/cart/CartCard.js
@@ -1,19 +1,76 @@
-import { StyleSheet, Text, View, Image } from 'react-native'
+import { StyleSheet, Text, View, Image, Dimensions } from 'react-native'
 import React, { useState } from 'react'
 import QtyButton from './QtyButton'
+import colours from '../../colours'
 
-const cartCard = props => {
+const CartCard = ({image, quant, name, ID}) => {
   const [quantity, setQuantity] = useState(0)
   return (
-    <View>
-      <Image/>
-      <Text>ItemName</Text>
-      <Text>IDNumber</Text>
-      <QtyButton />
+    <View style={styles.containerMain} >
+      <Image style={styles.prodImage} source={image} resizeMode="cover" accessibilityLabel={name}/>
+      <View style={styles.container}>
+        <View style={styles.containerDetails} >
+          <Text style={styles.name} adjustsFontSizeToFit={true}>{name}</Text>
+          <Text style={styles.prodID} >Product ID: {ID}</Text>
+        </View>
+        <QtyButton style={styles.quantBtn} quantity={quant} />
+      </View>
     </View>
   )
 }
 
-export default cartCard
+export default CartCard
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  containerMain:{
+    width: Dimensions.get('window').width * 0.9,
+    height: Dimensions.get('window').height * 0.2,
+    margin: '3%',
+    borderColor: colours.green,
+    borderRadius: 10,
+    borderWidth: 3,
+    backgroundColor: colours.cream,
+    flexDirection: 'row',
+    alignItems: 'center',
+    shadowColor: "#000",
+    shadowOffset: {
+        width: 0,
+        height: 1,
+    },
+    shadowOpacity: 0.22,
+    shadowRadius: 2.22,
+    elevation: 3,
+
+  },
+  prodImage:{
+    height: '90%',
+    flex: 4,
+    margin: '3%',
+    borderRadius: 10,
+  },
+  container:{
+    height: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 7,
+  },
+  containerDetails:{
+    flex: 2,
+
+  },
+  quantBtn:{
+    flex: 1,
+
+  },
+  name:{
+    fontFamily: 'Manrope_600SemiBold',
+    fontSize: 20,
+    color: colours.darkest,
+  },
+  prodID:{
+    fontFamily: 'Manrope_300Light',
+    fontSize: 10,
+    color: colours.darkest,
+  }
+})
--- components/cart/QtyButton.js
+++ components/cart/QtyButton.js
@@ -1,18 +1,16 @@
-import { StyleSheet, Button, Pressable, View, Text } from 'react-native'
+import { StyleSheet, Button, Pressable, View, Text, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
+import { Icon } from '@rneui/themed'
+import colours from '../../colours'
 
 const QtyButton = props => {
     const itemQuant = props.quantity
     const[quantity, setQuantity] = useState(itemQuant)
   return (
     <View style={styles.buttonView}>
-        <Pressable onPress={() => setQuantity(quantity + 1)}>
-            <Text>+</Text>
-        </Pressable>
-        <Text>{quantity}</Text>
-        <Pressable onPress={() => setQuantity(quantity - 1)}>
-            <Text>-</Text>
-        </Pressable>
+        <TouchableOpacity onPress={() => setQuantity(quantity + 1)}><Icon name='plus-circle' type='font-awesome' color={colours.blue} /></TouchableOpacity>
+        <Text style={styles.quant} >{quantity}</Text>
+        <TouchableOpacity onPress={() => setQuantity(quantity - 1)}><Icon name='minus-circle' type='font-awesome' color={colours.blue} /></TouchableOpacity>
     </View>
   )
 }
@@ -21,7 +19,14 @@ export default QtyButton
 
 const styles = StyleSheet.create({
   buttonView: {
-
+    flexDirection: 'row',
+    padding: '4%',
+    alignItems: 'center'
+  },
+  quant:{
+    color: colours.darkest,
+    padding: '3%',
+    fontSize: 50
   }
     
 })
