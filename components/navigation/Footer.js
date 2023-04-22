8334f87eae1656462a24a553934deca0d7378064
Alyssa <alyssa@kvasl.com>
2023-04-22 15:45:01 +1200

    Footer Component Completted

--- components/navigation/Footer.js
+++ components/navigation/Footer.js
@@ -1,22 +1,42 @@
-import { StyleSheet, Text, View, FlatList } from 'react-native'
+import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native'
 import React from 'react'
-
+import colours from '../../colours'
+import MyH2 from '../text/MyH2'
 import Bridge from '../smallElements/Bridge'
 
 const Footer = () => {
   return (
     <View>
-      <Bridge colour='black'/>
-      <View >
-        <Text>home</Text>
-        <Text>cart</Text>
-        <Text>shop</Text>
+      <Bridge colour={colours.brick} backgroundColor={colours.blue} />
+      <View style={styles.footerOptions}>
+        <TouchableOpacity><MyH2 colour={colours.darkest}>home</MyH2></TouchableOpacity>
+        <MyH2 colour={colours.darkest}>|</MyH2>
+        <TouchableOpacity><MyH2 colour={colours.darkest}>cart</MyH2></TouchableOpacity>
+        <MyH2 colour={colours.darkest}>|</MyH2>
+        <TouchableOpacity><MyH2 colour={colours.darkest}>shop</MyH2></TouchableOpacity>
       </View>
-      <View><Text>alyssapilbrow</Text></View>
+      <View style={styles.copyrightContainer}><Text style={{color: colours.orange}}>©alyssapilbrow2023</Text></View>
     </View>
   )
 }
 
 export default Footer
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  footerOptions:{
+    backgroundColor: colours.green,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingTop: '5%',
+    paddingBottom: '5%',
+    paddingLeft: '3%',
+    paddingRight: '3%'
+  },
+  copyrightContainer:{
+    backgroundColor: colours.yellow,
+    alignItems: 'center',
+    paddingTop: '1%',
+    paddingBottom: '1%'
+  }
+})
